dedcecd58b2665af9979c8f08d95de8fef72944f
r-kanamaru <ryouji0623.k@gmail.com>
2019-07-04 17:32:29 +0900

    true

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module SyoutengaiTsurumibashi
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
   end
 end
